fea5c1eededac9d1fdbb6976deb9082ce92fdcac
fabio <fabio@fabio-X555LN>
2018-05-16 22:43:50 +0200

    v2.1.1

--- LaBaionettaUpdater/it/polito/tdpProvaFinale/baionettaUpdater/dao/DBConnect.java
+++ LaBaionettaUpdater/it/polito/tdpProvaFinale/baionettaUpdater/dao/DBConnect.java
@@ -8,21 +8,22 @@ import com.mchange.v2.c3p0.DataSources;
 
 public class DBConnect {
 
-	private static String jdbcURL = "jdbc:mysql://localhost/baionetta" ;
+	private static String jdbcURL = "jdbc:mysql://localhost/baionetta";
 
 	private static DataSource ds ;
 
 	public static Connection getConnection() {
 
 		if(ds==null) {
+			// initialize DataSource
 			try {
-				ds = DataSources.pooledDataSource(DataSources.unpooledDataSource(jdbcURL, "fabio", "gtik9328")) ;
+				ds = DataSources.pooledDataSource(DataSources.unpooledDataSource(jdbcURL,"fabio", "gtik9328")) ;
 			} catch (SQLException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 				System.exit(1);
 			}
 		}
-
 		try {
 			Connection c = ds.getConnection() ;
 			return c ;
@@ -31,7 +32,5 @@ public class DBConnect {
 			e.printStackTrace();
 			return null ;
 		}
-
 	}
-
 }
